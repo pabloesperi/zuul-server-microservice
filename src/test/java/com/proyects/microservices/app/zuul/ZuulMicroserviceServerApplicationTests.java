4a52e92975cdcf2f4b2b9ffc49968484d4accf4c
pabloesperi <pabloesperi@hotmail.com>
2020-09-22 12:48:55 +0200

    first commit

---
+++ src/test/java/com/proyects/microservices/app/zuul/ZuulMicroserviceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyects.microservices.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulMicroserviceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
